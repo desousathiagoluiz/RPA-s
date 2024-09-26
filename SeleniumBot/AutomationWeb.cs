e172ba3078bfd496eb1e8a59ccda30ff6de9b59f
ThiaGão <desousathiagoluiz@gmail.com>
2024-09-26 17:06:28 -0300

    SeleniumBot

---
+++ SeleniumBot/AutomationWeb.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI; // para WebDriverWait
+using OpenQA.Selenium.Interactions; // para utilização do Actions
+using System;
+
+namespace SeleniumBot
+{
+    public class AutomationWeb
+    {
+        public IWebDriver driver;
+
+        public AutomationWeb()
+        {
+            // iniciar o ChromeDriver e maximiza a janela
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        public string TestWeb()
+        {
+            // navega no Google
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            // espera até o campo da pesquisa sempre estar visível e inserir o texto
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement searchBox = wait.Until(driver => driver.FindElement(By.Name("q")));
+            searchBox.SendKeys("Hello World");
+
+            // usa Js para clicar fora da caixa de sugestão e garanti que o botão esta clicável
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("document.activeElement.blur();"); // tira o foco campo de pesquisa
+
+            // aguarda que o botão esteja visível
+            IWebElement searchButton = wait.Until(driver => driver.FindElement(By.Name("btnK")));
+
+            // clica no botão de pesquisa usando Js - evita o problema de sobreposição
+            js.ExecuteScript("arguments[0].click();", searchButton);
+
+            // espera o resultado de pesquisa aparecer
+            IWebElement result = wait.Until(driver => driver.FindElement(By.XPath("//*[@id='rso']/div[3]/div/div/div/div[2]/div")));
+
+            // retorna o texto do resultado
+            var text = result.Text;
+
+            return text;
+        }
+
+        public void CloseBrowser()
+        {
+            // fecha o navegador
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumBot/Program.cs
@@ -0,0 +1,22 @@
+﻿using SeleniumBot;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // cria uma instancia da automação
+        var web = new AutomationWeb();
+
+        // executa o teste na web
+        var text = web.TestWeb();
+
+        // exibi o resultado no console
+        Console.WriteLine(text);
+
+        // fecha o navegador
+        web.CloseBrowser();
+
+        // mantem o console aberto
+        Console.ReadLine();
+    }
+}
